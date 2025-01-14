dd8aadb07e66796ad2531e60085a3ed2c0613644
KingMelo <kjpenson2@gmail.com>
2025-01-14 07:52:27 -0600

    recovered lost project

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from storages.backends.s3boto3 import S3Boto3Storage
+import uuid
+import hashlib
+import os
+from django.utils.text import slugify
+
 
 class Note(models.Model):
     title = models.CharField(max_length=100)
@@ -9,12 +15,96 @@ class Note(models.Model):
 
     def __str__(self):
         return self.title
-
-
-class BlocklistItem(models.Model):
-    ip_or_domain = models.CharField(max_length=255, unique=True)
-    added_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True) # Fixed on_delete
-    added_on = models.DateTimeField(auto_now_add=True)
     
+def upload_to(instance, filename):
+    base, extension = os.path.splitext(filename)
+    new_filename = f"{slugify(instance.title)}{extension}"
+    return os.path.join('pdfs/', new_filename)
+
+def upload_tracking_mail_receipt(instance, filename):
+    base, extension = os.path.splitext(filename)
+    new_filename = f"{slugify(instance.title)}-tracking-mail-receipt{extension}"
+    return os.path.join('tracking_mail_receipts/', new_filename)
+
+def upload_return_receipt(instance, filename):
+    base, extension = os.path.splitext(filename)
+    new_filename = f"{slugify(instance.title)}-return-receipt{extension}"
+    return os.path.join('return_receipts/', new_filename)
+
+class Record(models.Model):
+    RECORD_TYPE_CHOICES = [
+        ('cafv', 'Conditional_Acceptance_for_Value'),
+        ('ipn', 'International_Promissory_Note'),
+        ('boe', 'International_Bill_of_Exchange'),
+        ('noticeBreach', 'Notice_of_Breach'),
+        ('noticeCure', 'Notice_of_Cure'),
+        ('noticeDefault', 'Notice_of_Default'),
+        ('tort', 'Tort_Claim'),
+        ('noticeDemand', 'Notice_of_Demand'),
+        ('noticeDispute', 'Notice_of_Dispute'),
+        ('noticeFraud', 'Notice_of_Fraud'),
+        ('executor', 'Executor_Letter'),
+        ('crn', 'Copyright_Notice'),
+    ]
+
+    TRACKING_TYPE_CHOICES = [
+        ('certifiedmail_usps', 'Certified Mail USPS'),
+        ('registeredmail_usps', 'Registered Mail USPS'),
+    ]
+
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('delivered', 'Delivered'),
+        ('confirmation', 'Confirmation'),
+    ]
+
+    title = models.CharField(max_length=255, editable=False, verbose_name="Title")
+    record_type = models.CharField(max_length=20, choices=RECORD_TYPE_CHOICES, default='default', verbose_name="Record Type")
+    tracking_number = models.CharField(max_length=100, blank=True, null=True, verbose_name="Tracking Number")
+    tracking_type = models.CharField(max_length=20, choices=TRACKING_TYPE_CHOICES, blank=True, null=True, verbose_name="Tracking Type")
+    return_receipt = models.CharField(max_length=100, blank=True, null=True, verbose_name="Return Receipt")
+    blockchain_record = models.JSONField(default=list, verbose_name="Blockchain Record")  # Start with an empty list, can be extended later
+    record_location = models.JSONField(default=list, verbose_name="Record Location")  # Array of objects for cloud bucket locations, local storage reference, etc.
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, verbose_name="UUID")
+    related_documents = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='related_records', verbose_name="Related Documents")
+    company_name = models.CharField(max_length=255, blank=True, null=True, verbose_name="Company Name")
+    ceo = models.CharField(max_length=255, blank=True, null=True, verbose_name="CEO")
+    cfo = models.CharField(max_length=255, blank=True, null=True, verbose_name="CFO")
+    mailing_address = models.CharField(max_length=255, blank=True, null=True, verbose_name="Mailing Address")
+    city = models.CharField(max_length=100, blank=True, null=True, verbose_name="City")
+    state = models.CharField(max_length=100, blank=True, null=True, verbose_name="State")
+    zip = models.CharField(max_length=20, blank=True, null=True, verbose_name="ZIP Code")
+    email = models.EmailField(max_length=255, blank=True, null=True, verbose_name="Email")
+    phone_number = models.CharField(max_length=20, blank=True, null=True, verbose_name="Phone Number")
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='created_records', verbose_name="Created By")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At")  # Timestamp for when the record is created
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated At")  # Timestamp for when the record is last updated
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending', verbose_name="Status")
+    is_deleted = models.BooleanField(default=False, verbose_name="Is Deleted")  # Soft delete flag
+    pdf_file = models.FileField(storage=S3Boto3Storage(bucket_name='omnistance-openrecord-pdfs'), upload_to=upload_to, blank=True, null=True, verbose_name="PDF File")
+    tracking_mail_receipt = models.FileField(storage=S3Boto3Storage(bucket_name='omnistance-openrecord-tracking'), upload_to=upload_tracking_mail_receipt, blank=True, null=True, verbose_name="Tracking Mail Receipt")
+    return_receipt_file = models.FileField(storage=S3Boto3Storage(bucket_name='omnistance-openrecord-greencards'), upload_to=upload_return_receipt, blank=True, null=True, verbose_name="Return Receipt File")
+    hash = models.CharField(max_length=64, blank=True, null=True, verbose_name="SHA256")
+    
+
+    def generate_hash(self, file):
+        hasher = hashlib.sha256()
+        for chunk in file.chunks():
+            hasher.update(chunk)
+        return hasher.hexdigest()
+
+
+    def save(self, *args, **kwargs):
+        # Generate the title
+        self.title = f"{self.get_record_type_display()}_{self.tracking_number or 'No Tracking Number'}_{self.company_name or 'No Company Name'}"
+
+        # Generate the hash if a PDF file is uploaded
+        if self.pdf_file:
+            self.hash = self.generate_hash(self.pdf_file)
+
+        super().save(*args, **kwargs)
+    
+
+
     def __str__(self):
-        return self.ip_or_domain
+        return self.title
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from rest_framework import generics, viewsets, permissions, status
-from .serializers import UserSerializer, NoteSerializer, BlocklistItemSerializer
+from .serializers import UserSerializer, NoteSerializer, RecordSerializer
 from django.http import JsonResponse
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
-from rest_framework.views import APIView  # Add this line
+from rest_framework.views import APIView
 from rest_framework.parsers import JSONParser
-from .models import Note, BlocklistItem
-import os
-import ipaddress
-import re
+from .models import Note, Record
 
 
 class NoteListCreate(generics.ListCreateAPIView):
@@ -36,61 +33,23 @@ class NoteDelete(generics.DestroyAPIView):
         return Note.objects.filter(author=user)
 
 
-# Create your views here.
+
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
 
-class BlocklistView(APIView):
-    permission_classes = [AllowAny]
+class RecordListCreate(generics.ListCreateAPIView):
+    queryset = Record.objects.all()
+    serializer_class = RecordSerializer
+    permission_classes = [IsAuthenticated]
 
-    def get_blocklist_path(self):
-        base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        blocklist_path = os.path.join(base_dir, 'blocklist.txt')
-        return blocklist_path
+class RecordDetailUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Record.objects.all()
+    serializer_class = RecordSerializer
+    permission_classes = [IsAuthenticated]
 
-    def get(self, request):
-        blocklist_path = self.get_blocklist_path()
-        with open(blocklist_path, 'r') as f:
-            blocklist = f.read().splitlines()
-        return JsonResponse({'blocklist': blocklist})
-    
-    def post(self, request):
-        # Parse the request data
-        data = JSONParser().parse(request)
-        serializer = BlocklistItemSerializer(data=data)
-        if serializer.is_valid():
-            blocklist_path = self.get_blocklist_path()
-            ip_or_domain = serializer.validated_data['ip_or_domain']
-            with open(blocklist_path, 'r+') as file:
-                blocklist = file.read().splitlines()
-                if ip_or_domain in blocklist:
-                    return JsonResponse({'status': 'error', 'message': 'IP or domain already in blocklist'}, status=400)
-                else:
-                    file.write(f'{ip_or_domain}\n')
-                    return JsonResponse({'status': 'success', 'ip_or_domain': ip_or_domain}, status=201)
-
-    def delete(self, request):
-        # Parse the request data
-        data = JSONParser().parse(request)
-        serializer = BlocklistItemSerializer(data=data)
-        if serializer.is_valid():
-            blocklist_path = self.get_blocklist_path()
-            ip_or_domain = serializer.validated_data['ip_or_domain']
-            # Read the current blocklist
-            with open(blocklist_path, 'r') as file:
-                lines = file.read().splitlines()
-            # Check if ip_or_domain is in the blocklist
-            if ip_or_domain in lines:
-                # Remove ip_or_domain
-                lines.remove(ip_or_domain)
-                # Write the updated blocklist back to the file
-                with open(blocklist_path, 'w') as file:
-                    for line in lines:
-                        file.write(f'{line}\n')
-                return JsonResponse({'status': 'success', 'action': 'removed', 'ip_or_domain': ip_or_domain})
-            else:
-                return JsonResponse({'status': 'error', 'message': 'Item not found in blocklist'}, status=404)
-        else:
-            return JsonResponse({'status': 'error', 'errors': serializer.errors}, status=400)
+class RecordView(generics.RetrieveAPIView):
+    queryset = Record.objects.all()
+    serializer_class = RecordSerializer
+    permission_classes = [IsAuthenticated]
