b402d4e7ff4679d371fe89489fc213081c14f22d
KingMelo <kjpenson2@gmail.com>
2025-01-14 07:52:27 -0600

    recovered lost project

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Note, BlocklistItem
+from .models import Note, Record
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,12 +17,8 @@ class NoteSerializer(serializers.ModelSerializer):
         model = Note
         fields = ["id", "title", "content", "created_at", "author"]
         extra_kwargs = {"author": {"read_only": True}}
-
-class BlocklistItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BlocklistItem
-        fields = ['ip_or_domain', 'added_by', 'added_on']
     
-    def validate_ip_or_domain(self, value):
-        #Validation
-        return value
+class RecordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Record
+        fields = '__all__'
